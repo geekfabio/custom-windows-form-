07adcf44946ff5e4b77b579df9c67ea2d6b84734
Trindade <Trindade@DESKTOP-7BB5H88>
2018-02-02 15:31:17 +0100

    Add project files.

---
+++ Vissoka Booster/VissokaForm.designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Vissoka_Booster
+{
+    partial class VissokaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TopBorderPanel = new System.Windows.Forms.Panel();
+            this.RightPanel = new System.Windows.Forms.Panel();
+            this.LeftPanel = new System.Windows.Forms.Panel();
+            this.BottomPanel = new System.Windows.Forms.Panel();
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.WindowTextLabel = new System.Windows.Forms.Label();
+            this._MaxButton = new System.Windows.Forms.Button();
+            this._MinButton = new System.Windows.Forms.Button();
+            this._CloseButton = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.TopPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TopBorderPanel
+            // 
+            this.TopBorderPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(20)))), ((int)(((byte)(50)))));
+            this.TopBorderPanel.Cursor = System.Windows.Forms.Cursors.SizeNS;
+            this.TopBorderPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopBorderPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopBorderPanel.Name = "TopBorderPanel";
+            this.TopBorderPanel.Size = new System.Drawing.Size(724, 2);
+            this.TopBorderPanel.TabIndex = 0;
+            this.TopBorderPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopBorderPanel_MouseDown);
+            this.TopBorderPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopBorderPanel_MouseMove);
+            this.TopBorderPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TopBorderPanel_MouseUp);
+            // 
+            // RightPanel
+            // 
+            this.RightPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(20)))), ((int)(((byte)(30)))));
+            this.RightPanel.Cursor = System.Windows.Forms.Cursors.SizeWE;
+            this.RightPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.RightPanel.Location = new System.Drawing.Point(722, 2);
+            this.RightPanel.Name = "RightPanel";
+            this.RightPanel.Size = new System.Drawing.Size(2, 475);
+            this.RightPanel.TabIndex = 1;
+            this.RightPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.RightPanel_MouseDown);
+            this.RightPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.RightPanel_MouseMove);
+            this.RightPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.RightPanel_MouseUp);
+            // 
+            // LeftPanel
+            // 
+            this.LeftPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(20)))), ((int)(((byte)(30)))));
+            this.LeftPanel.Cursor = System.Windows.Forms.Cursors.SizeWE;
+            this.LeftPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftPanel.Location = new System.Drawing.Point(0, 2);
+            this.LeftPanel.Name = "LeftPanel";
+            this.LeftPanel.Size = new System.Drawing.Size(2, 475);
+            this.LeftPanel.TabIndex = 2;
+            this.LeftPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.LeftPanel_MouseDown);
+            this.LeftPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LeftPanel_MouseMove);
+            this.LeftPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.LeftPanel_MouseUp);
+            // 
+            // BottomPanel
+            // 
+            this.BottomPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(20)))), ((int)(((byte)(30)))));
+            this.BottomPanel.Cursor = System.Windows.Forms.Cursors.SizeNS;
+            this.BottomPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BottomPanel.Location = new System.Drawing.Point(2, 475);
+            this.BottomPanel.Name = "BottomPanel";
+            this.BottomPanel.Size = new System.Drawing.Size(720, 2);
+            this.BottomPanel.TabIndex = 3;
+            this.BottomPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.BottomPanel_MouseDown);
+            this.BottomPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.BottomPanel_MouseMove);
+            this.BottomPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.BottomPanel_MouseUp);
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(40)))), ((int)(((byte)(60)))));
+            this.TopPanel.Controls.Add(this.WindowTextLabel);
+            this.TopPanel.Controls.Add(this._MaxButton);
+            this.TopPanel.Controls.Add(this._MinButton);
+            this.TopPanel.Controls.Add(this._CloseButton);
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(2, 2);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(720, 35);
+            this.TopPanel.TabIndex = 4;
+            this.TopPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseDown);
+            this.TopPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseMove);
+            this.TopPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseUp);
+            // 
+            // WindowTextLabel
+            // 
+            this.WindowTextLabel.AutoSize = true;
+            this.WindowTextLabel.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WindowTextLabel.ForeColor = System.Drawing.Color.White;
+            this.WindowTextLabel.Location = new System.Drawing.Point(10, 8);
+            this.WindowTextLabel.Name = "WindowTextLabel";
+            this.WindowTextLabel.Size = new System.Drawing.Size(44, 20);
+            this.WindowTextLabel.TabIndex = 6;
+            this.WindowTextLabel.Text = "Form";
+            // 
+            // _MaxButton
+            // 
+            this._MaxButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this._MaxButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this._MaxButton.FlatAppearance.BorderSize = 0;
+            this._MaxButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this._MaxButton.ForeColor = System.Drawing.Color.White;
+            this._MaxButton.Image = global::Vissoka_Booster.Properties.Resources.form_mOff;
+            this._MaxButton.Location = new System.Drawing.Point(644, 3);
+            this._MaxButton.Name = "_MaxButton";
+            this._MaxButton.Size = new System.Drawing.Size(35, 25);
+            this._MaxButton.TabIndex = 5;
+            this.toolTip1.SetToolTip(this._MaxButton, "Maximize");
+            this._MaxButton.UseVisualStyleBackColor = true;
+            this._MaxButton.Click += new System.EventHandler(this._MaxButton_Click);
+            // 
+            // _MinButton
+            // 
+            this._MinButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this._MinButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this._MinButton.FlatAppearance.BorderSize = 0;
+            this._MinButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this._MinButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._MinButton.ForeColor = System.Drawing.Color.White;
+            this._MinButton.Image = global::Vissoka_Booster.Properties.Resources.formMinimus;
+            this._MinButton.Location = new System.Drawing.Point(609, 3);
+            this._MinButton.Name = "_MinButton";
+            this._MinButton.Size = new System.Drawing.Size(35, 25);
+            this._MinButton.TabIndex = 1;
+            this._MinButton.Text = "_";
+            this.toolTip1.SetToolTip(this._MinButton, "Minimize");
+            this._MinButton.UseVisualStyleBackColor = true;
+            this._MinButton.Click += new System.EventHandler(this._MinButton_Click);
+            // 
+            // _CloseButton
+            // 
+            this._CloseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this._CloseButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this._CloseButton.FlatAppearance.BorderSize = 0;
+            this._CloseButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this._CloseButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Crimson;
+            this._CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this._CloseButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._CloseButton.ForeColor = System.Drawing.Color.White;
+            this._CloseButton.Image = global::Vissoka_Booster.Properties.Resources.form_close;
+            this._CloseButton.Location = new System.Drawing.Point(679, 3);
+            this._CloseButton.Name = "_CloseButton";
+            this._CloseButton.Size = new System.Drawing.Size(35, 25);
+            this._CloseButton.TabIndex = 0;
+            this.toolTip1.SetToolTip(this._CloseButton, "Close");
+            this._CloseButton.UseVisualStyleBackColor = true;
+            this._CloseButton.Click += new System.EventHandler(this._CloseButton_Click);
+            // 
+            // VissokaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(724, 477);
+            this.Controls.Add(this.TopPanel);
+            this.Controls.Add(this.BottomPanel);
+            this.Controls.Add(this.LeftPanel);
+            this.Controls.Add(this.RightPanel);
+            this.Controls.Add(this.TopBorderPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "VissokaForm";
+            this.Text = "Vissoka Form";
+            this.Load += new System.EventHandler(this.BlueForm_Load);
+            this.TextChanged += new System.EventHandler(this.VissokaForm_TextChanged);
+            this.TopPanel.ResumeLayout(false);
+            this.TopPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel TopBorderPanel;
+        private System.Windows.Forms.Panel RightPanel;
+        private System.Windows.Forms.Panel LeftPanel;
+        private System.Windows.Forms.Panel BottomPanel;
+        private System.Windows.Forms.Panel TopPanel;
+        private System.Windows.Forms.Button _CloseButton;
+        private System.Windows.Forms.Button _MaxButton;
+        private System.Windows.Forms.Button _MinButton;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label WindowTextLabel;
+    }
+}
